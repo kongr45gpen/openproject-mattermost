3623229e0fe5933318b8925ff43dac9777945cef
kongr45gpen <electrovesta@gmail.com>
2020-07-09 16:26:50 +0000

    What??

--- lib/open_project/mattermost/hooks.rb
+++ lib/open_project/mattermost/hooks.rb
@@ -3,7 +3,7 @@ module OpenProject::Mattermost
 
     def mattermost_event_notification(context = {})
       event = MattermostEvent.find_or_create_by(context)
-      SendMattermostMessageJob.set(wait_until: 5.seconds.from_now).perform_later(event)
+      SendMattermostMessageJob.set(wait_until: 20.seconds.from_now).perform_later(event)
     end
   end
 end
